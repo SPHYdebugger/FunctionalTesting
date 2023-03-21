7937d739f9c12371e07edf5fc64cb215af279ae3
sphy <sphy.debugger@gmail.com>
2023-03-21 18:10:36 +0100

    Chromedriver is running

--- src/main/java/MainLidl.java
+++ src/main/java/MainLidl.java
@@ -1,22 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
 
 
 public class MainLidl {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
-
 
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.lidl.es");
 
 
-        driver.quit();
     }
 
 }
