a8018ebf640c3a7bd8d13a979b303ca98b8007c6
sphy <sphy.debugger@gmail.com>
2023-03-21 17:56:32 +0100

    The page does not open with Chromedriver

--- src/main/java/MainLidl.java
+++ src/main/java/MainLidl.java
@@ -1,6 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class MainLidl {
 
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://www.lidl.es");
+
+
+        driver.quit();
+    }
 
 }
